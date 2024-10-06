61bf1cee1022cfae1a204ce1525177f255043e5e
VincentBaron <vbaron@student.42.fr>
2024-10-06 14:30:06 +0200

    feat: Added auth handler

---
+++ backend/internal/handlers/auth.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/VincentBaron/bangr/backend/internal/models"
+	"github.com/VincentBaron/bangr/backend/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	authService *services.AuthService
+}
+
+func NewAuthHandler(authService *services.AuthService) *AuthHandler {
+	return &AuthHandler{
+		authService: authService,
+	}
+}
+
+func (h *AuthHandler) CallbackHandler(c *gin.Context) {
+	code := c.Query("code")
+	state := c.Query("state")
+
+	if code == "" || state == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read query params",
+		})
+		return
+	}
+
+	if err := h.authService.CallbackService(c); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.Redirect(http.StatusTemporaryRedirect, "http://localhost:5173")
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	// Get email & pass off req body
+	var body struct {
+		Username string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	err := h.authService.Login(c, body.Username, body.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+func (h *AuthHandler) Signup(c *gin.Context) {
+	// Get the email/pass off req Body
+	var payload models.User
+
+	if c.Bind(&payload) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	url, err := h.authService.Signup(c, &payload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	// // Respond
+	c.JSON(http.StatusOK, gin.H{"url": url})
+}
