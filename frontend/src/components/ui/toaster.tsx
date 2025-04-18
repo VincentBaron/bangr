484da8753dd726b9f3c8b383008c579bd02e380e
VincentBaron <vbaron@student.42.fr>
2025-04-18 16:25:49 +0200

    fix: tsc linting

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -1,4 +1,5 @@
-import { useToast } from "@/components/hooks/use-toast"
+import { useToast } from "../../hooks/use-toast";
+import type { ToasterToast } from "../../hooks/use-toast";
 import {
   Toast,
   ToastClose,
@@ -6,28 +7,30 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
+} from "@/components/ui/toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function (toast: ToasterToast) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={toast.id} {...toast}>
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
+              {toast.title && <ToastTitle>{toast.title}</ToastTitle>}
+              {toast.description && (
+                <ToastDescription>{toast.description}</ToastDescription>
               )}
             </div>
-            {action}
+            {toast.action && (
+              <div className="flex items-center space-x-2">{toast.action}</div>
+            )}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
